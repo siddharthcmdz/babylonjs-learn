e6ee7e4a1bc3c69d4bf45c105aacfaa538e3bdfc
Siddharth Palaniappan <siddharth.cmdz@gmail.com>
2023-03-31 14:48:50 -0700

    using webpack html template creation

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,22 @@
 const path = require("path")
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: "development",
     entry: "./src/index.js",
     output: {
-        filename: "main.js",
+        filename: 'main.[hash].js',
         path: path.resolve(__dirname, "dist")
     },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: './src/template.html'
+    })],
 }
