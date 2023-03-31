97192735dad5342519cb1fc6bf4de68e561dcfbe
Siddharth Palaniappan <siddharth.cmdz@gmail.com>
2023-03-30 19:26:50 -0700

    working code of babylon with webpack

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-import { ArcRotateCamera, Engine, Scene, HemisphericLight, MeshBuilder } from '@babylonjs/core'
-
-console.log('hello from webpack')
+import { ArcRotateCamera, Engine, Scene, HemisphericLight, MeshBuilder, Vector3 } from '@babylonjs/core'
 
 const createScene = (canvas, engine) => {
     const scene = new Scene(engine);
-    const camera = new ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new BABYLON.Vector3(0, 0, 0));
+    const camera = new ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new Vector3(0, 0, 0));
     camera.attachControl(canvas, true);
 
-    const light = new HemisphericLight("light", new BABYLON.Vector3(0, 1, 0));
+    const light = new HemisphericLight("light", new Vector3(0, 1, 0));
 
     const box = MeshBuilder.CreateBox("box", {});
 
@@ -16,9 +14,13 @@ const createScene = (canvas, engine) => {
 
 const init = () => {
     const canvas = document.getElementById("renderCanvas"); // Get the canvas element
-
+    console.log('canvas', canvas)
+    if (canvas === null) {
+        return;
+    }
     const engine = new Engine(canvas, true); // Generate the BABYLON 3D engine
     const scene = createScene(canvas, engine);
+    console.log('scene', scene)
     engine.runRenderLoop(function () {
         scene.render();
     })
@@ -27,3 +29,9 @@ const init = () => {
         engine.resize();
     })
 }
+
+init()
+
+console.log('hello from webpack')
+const canvas = document.getElementById("renderCanvas"); // Get the canvas element
+console.log(canvas)
